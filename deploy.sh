47e3db31235684235cf8c18da5452eb3fbc7bd0f
Aidan Feldman <aidan.feldman@gmail.com>
2018-02-08 00:24:31 -0800

    ensure SSH agent has all keys

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,17 @@
 set -e
 set -x
 
+# ensure SSH agent has all keys
+ssh-add -A
+
+# set up the infrastructure
 cd terraform
 terraform init
 terraform apply -auto-approve
 
 cd ../ansible
 pip install -r requirements.txt
+# pull the instance information from Terraform, and run the Ansible playbook against it to configure
 TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision.yml
 
 echo "Success!"
