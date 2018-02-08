a0889812bd301c2237a6137695fc5d5c5e2d508c
Aidan Feldman <aidan.feldman@gmail.com>
2018-02-07 23:27:10 -0800

    disable host key checking in Ansible config file
    ...to simplify deploy script.

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ terraform apply
 
 cd ../ansible
 pip install -r requirements.txt
-ANSIBLE_HOST_KEY_CHECKING=False TF_STATE=../terraform/terraform.tfstate ansible-playbook --inventory-file=$(which terraform-inventory) provision.yml
+TF_STATE=../terraform/terraform.tfstate ansible-playbook "--inventory-file=$(which terraform-inventory)" provision.yml
 
 echo "Success!"
 
